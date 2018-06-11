29db4f82f9c988bd48af2785b0b9121aa10acb3c
Orman <CDO8081@yum.com>
2018-06-11 10:59:20 -0400

    Add activity functions

---
+++ DurableFunctionExample/ProcessVideoActivities.cs
@@ -0,0 +1,47 @@
+﻿using System.Configuration;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace DurableFunctionExample
+{
+    public class ProcessVideoActivities
+    {
+        [FunctionName("A_TranscodeVideo")]
+        public static async Task<string> TranscodeVideo(
+            [ActivityTrigger] string inputVideo,
+            TraceWriter log)
+        {
+            log.Info($"Transcoding {inputVideo}");
+            //simulate doing the activity
+            await Task.Delay(5000);
+
+            return "transcoded.mp4";
+        }
+
+        [FunctionName("A_ExtractThumbnail")]
+        public static async Task<string> ExtractThumbnail(
+            [ActivityTrigger] string inputVideo,
+            TraceWriter log)
+        {
+            log.Info($"Extracting Thumbnail {inputVideo}");
+            //simulate doing the activity
+            await Task.Delay(5000);
+
+            return "thumbnail.png";
+        }
+
+        [FunctionName("A_PrependIntro")]
+        public static async Task<string> PrependIntro(
+            [ActivityTrigger] string inputVideo,
+            TraceWriter log)
+        {
+            log.Info($"Appending intro to video {inputVideo}");
+            var introLocation = ConfigurationManager.AppSettings["IntroLocation"];
+            //simulate doing the activity
+            await Task.Delay(5000);
+
+            return "withIntro.mp4";
+        }
+    }   
+}
---
+++ DurableFunctionExample/ProcessVideoOrchestrators.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace DurableFunctionExample
+{
+    public static class ProcessVideoOrchestrators
+    {
+        [FunctionName("O_ProcessVideo")]
+        public static async Task<object> ProcessVideo(
+            [OrchestrationTrigger] DurableOrchestrationContext ctx,
+            TraceWriter log)
+        {
+            var videoLocation = ctx.GetInput<string>();
+
+            var transcodedLocation = await ctx.CallActivityAsync<string>("A_TranscodeVideo", videoLocation);
+
+            var thumbnailLocation = await ctx.CallActivityAsync<string>("A_ExtractThumbnail", transcodedLocation);
+
+            var withIntoLocation = await ctx.CallActivityAsync<string>("A_PrependIntro", transcodedLocation);
+
+            return new
+            {
+                Transcoded = transcodedLocation,
+                Thumbnail = thumbnailLocation,
+                WithIntro = withIntoLocation
+            };
+        }
+    }
+}
